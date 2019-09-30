07453bfcadf608e708f75cfaedaf52d25f27de9f
sara riaz <srq147147@gmail.com>
2019-09-30 01:09:46 -0700

    nest-crud with typeOrm and mySql

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -14,7 +14,19 @@ export class ContactController {
 
     @Post('create')
     async create(@Body() contactData: Contact): Promise<any> {
-      return this.contactService.create(contactData);
+      return this.contactService.create(contactData);//We use the @Body decorator to extract and inject the body of the POST request in the create() method. 
+    }  
+
+    @Delete(':id/delete')
+    async delete(@Param('id') id): Promise<any> { //We extract and inject the id using the @Param() decorator and we call the delete() method of ContactsService.
+      return this.contactService.delete(id);
+    }  
+
+    @Put(':id/update')
+    async update(@Param('id') id, @Body() contactData: Contact): Promise<any> {
+        contactData.id = Number(id);
+        console.log('Update #' + contactData.id)
+        return this.contactService.update(contactData);
     }  
 
 }
