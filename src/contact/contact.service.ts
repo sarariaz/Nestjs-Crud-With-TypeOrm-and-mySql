ca8fdd0ba9f260822270a95b07246bd6e17843e2
sara riaz <srq147147@gmail.com>
2019-09-30 00:05:00 -0700

    nest-crud with typeOrm and mySql

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm'; // ye 4 khud add 
+import { InjectRepository } from '@nestjs/typeorm';
+import { Contact } from './contact.entity';
+import { UpdateResult, DeleteResult } from  'typeorm';
 
 @Injectable()
-export class ContactService {}
+export class ContactService {
+    constructor(
+        @InjectRepository(Contact)
+        private contactRepository: Repository<Contact>,
+    ) { }
+
+
+
+
+
+
+
+
+}
