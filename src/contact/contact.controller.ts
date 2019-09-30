bc5f254aedeb70f9d2720e346995e3b80d7ca1e2
sara riaz <srq147147@gmail.com>
2019-09-30 00:14:26 -0700

    nest-crud with typeOrm and mySql

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { Contact } from './contact.entity'; //ye 3 khud se
+import { ContactService } from './contact.service';
+import { Post,Put, Delete, Body, Param, Get } from  '@nestjs/common';
 
 @Controller('contact')
-export class ContactController {}
+export class ContactController {
+    constructor(private contactService: ContactService){}
+
+    @Get()
+    index(): Promise<Contact[]> {
+      return this.contactService.findAll();
+    } 
+
+}
+
